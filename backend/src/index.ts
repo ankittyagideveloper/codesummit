597224d97327c2bee8e535cbd1a6582e71965609
Ajay Patel <patel.ajay745@gmail.com>
2025-04-28 15:51:18 -0400

    backend setup

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config'
+import { env } from '@/validators/env';
+import connectDB from "@/db"
+import express from "express";
+import app from './app';
+
+const PORT = env.PORT ?? 8000
+
+app.listen(PORT, () => {
+    console.log(`server running on ${env.BASEURL}:${PORT}`)
+})
+
